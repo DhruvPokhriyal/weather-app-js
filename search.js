0b4b31a24bf9601bbcc3810549dd7bcf3cf12cbe
Dhruv Pokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2024-12-25 19:35:38 +0530

    Create feature to receive place name from searchbar and also added API KEY as constant

---
+++ search.js
@@ -0,0 +1,5 @@
+const API_KEY = "J4BL7R2WRB5A6KFZK64VGDPLN";
+
+const params = new URLSearchParams(window.location.search);
+const search = params.get("searchbar");
+document.querySelector("body").textContent = search;
