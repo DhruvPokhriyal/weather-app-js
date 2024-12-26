ecb6102da08a1d128d8c8d56d0a087d5f1a4662d
Dhruv Pokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2024-12-26 11:06:56 +0530

    Add icons to images/icons folder for different weather and add bg to search page

--- search.js
+++ search.js
@@ -2,4 +2,4 @@ const API_KEY = "J4BL7R2WRB5A6KFZK64VGDPLN";
 
 const params = new URLSearchParams(window.location.search);
 const search = params.get("searchbar");
-document.querySelector("body").textContent = search;
+// document.querySelector("body").textContent = search;
